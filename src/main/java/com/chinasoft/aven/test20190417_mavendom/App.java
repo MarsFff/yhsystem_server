db31699a7db711eb8985ca80923b669b0469ecdc
fff <172953907@qq.com>
2019-04-18 15:31:24 +0800

    第二次测试

---
+++ src/main/java/com/chinasoft/aven/test20190417_mavendom/App.java
@@ -0,0 +1,13 @@
+package com.chinasoft.aven.test20190417_mavendom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/chinasoft/aven/test20190417_mavendom/MyTest.java
@@ -0,0 +1,12 @@
+package com.chinasoft.aven.test20190417_mavendom;
+
+import org.junit.Test;
+
+import oracle.jdbc.driver.OracleDriver;
+
+public class MyTest {
+	@Test
+public void hello() {
+	System.out.println("sssssssssssssd");
+}
+}
